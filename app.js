820e0622078480756044285a3f74db7549f588a7
xJosafat <iwant_to_believe@hotmail.com>
2018-07-04 23:18:30 -0500

    My first angular app from scratch

---
+++ app.js
@@ -0,0 +1,9 @@
+angular.module("App",['main']);
+
+angular.module("main",[]);
+
+angular.module("main").controller("mainController",function($scope){
+	var data = "roger";
+
+	$scope.data = "that";
+});
